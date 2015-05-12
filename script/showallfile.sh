9fe80d8377e5f0208d1c2ac7bb6f21f88df1cbc2
amangupta0297 <aman.gupta.0297@gmail.com>
2015-05-12 15:23:31 +0530

    to show all files in a directory with their size if directory exist else exit

---
+++ script/showallfile.sh
@@ -0,0 +1,17 @@
+#show all file names in a directory with their size
+ 
+#take directory name from user
+#check directory name (if t exist then show all the files inside it with their size else exit)
+#!/bin/bash
+echo -n "enter DIRNAME :"
+read DIRNAME
+
+if [ ! -d $DIRNAME ]
+ then 
+      echo "dir does not exist. "
+      exit 0
+ else 
+      echo "dir exist"
+      echo "The files with their name and size :"
+      ls -al $DIRNAME|tail --line=+4|awk '{print  $5,"\t"$9}'
+fi 
