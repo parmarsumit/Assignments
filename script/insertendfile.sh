3349f9cbbae36ee2c6fa0466a0be89de1ba34eae
amangupta0297 <aman.gupta.0297@gmail.com>
2015-05-15 13:13:14 +0530

    modified script  to insert line a end of file

--- script/insertendfile.sh
+++ script/insertendfile.sh
@@ -1,6 +1,5 @@
 #insert a line in a file at position which user has entered
 #check for file if exist,then ask for line to be inserted and at which position to be entered, else exit
-#after entering position it will check if there are sufficient lines then it will insert that line at that position, else exit
 #!/bin/bash
 
 
@@ -12,7 +11,7 @@ read FILENAME
    echo "file exist"
    echo "enter line to be inserted"
    read LINE
-   echo "$LINE" >> $FILENAME  #position,line and filename are passed by user
+   echo "$LINE" >> $FILENAME  #line and filename are passed by user
    echo "the line is inserted "
  else
    echo "file does not exist"
