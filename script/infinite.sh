1b7b50caf984ca8639030ecd9eea9bb41b54eebb
aman <aman.gupta.0297@gmail.com>
2015-05-27 07:26:46 +0000

    modified script of creating 1kb unlimited files till disk get out of spaceand then fix it

--- script/infinite.sh
+++ script/infinite.sh
@@ -2,25 +2,28 @@
 # to create infinte files of 1kb till the disk get out of space and then remove all the files from it.
 check_space()
 {
-space=`df -a | awk 'NR==2{print $4}'`
+ space=`df -a | awk 'NR==2{print $4}'`
 }
+
 echo "space available on disk"
 check_space
 echo "$space"
+
 while :
-do 
-if [ "$space" -ne "0" ]
-then 
-fallocate -l 1k ${RANDOM}.txt  #create text file of 1kb
-check_space
-echo "$space"
-else
-echo "space on disk is full"
-check_space
-echo "$space"
-break
-fi
+   do 
+      if [ "$space" -ne "0" ]
+      then 
+        fallocate -l 1k ${RANDOM}.txt  #create text file of 1kb
+        check_space
+        echo "$space"
+      else
+        echo "space on disk is full"
+        check_space
+        echo "$space"
+      break
+      fi
 done
+
 find -name "*.txt" -size 1k -delete  # find and delete text file of 1kb
 echo "space available on disk"
 check_space
